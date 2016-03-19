74d46cb081bc242ee0bf9172ccb98e3f85c3750f
Defozo <defozo@gmail.com>
2016-03-19 08:39:59 +0100

    Changing random words works

--- plugin/dezinformator.js
+++ plugin/dezinformator.js
@@ -24,76 +24,50 @@ $(document).ready(function()
         {
             //alert("next <p> element: " + $(this)[0].innerText);
             var text = $(this)[0].innerText;
+            text = text.replace("&nbsp", " ");
+            text = text.replace("(", " ");
+            text = text.replace(")", " ");
+            text = text.replace("!", " ");
+            text = text.replace("?", " ");
+            text = text.replace(",", " ");
+            text = text.replace(".", " ");
+            text = text.replace("   ", " "); //nie wiem, czy potrzebne
+            text = text.replace("  ", " ");
             var splitted_text = text.split(" ");
             var text_without_short_words = [];
             splitted_text.forEach(function(element)
             {
                 if (element.length > 2) {
-                    element = element.replace(",", ""); //usuwanie przecinków
-                    element = element.replace(".", ""); //usuwanie kropek
                     text_without_short_words.push(element);
-                    //alert(element);
                 }
             });
-            var obecny = 5;
+            var obecny = 0;
             var przeskocz;
-            alert("length: "+text_without_short_words.length);
-            //while (obecny<=text_without_short_words.length) {
-                //przeskocz = getRandomInt(2,10);
-                //obecny += przeskocz;
-                //alert(document.getElementsByTagName('p').innerHTML);
-                //$("p").each(function()
-                //{
-                    //alert("myk");
-                    //$(this)[0].innerText = $(this)[0].innerText.replace(text_without_short_words[obecny], requestForChangedText(text_without_short_words[obecny]));
-                //document.body.innerHTML = document.body.innerHTML.replace(text_without_short_words[obecny], requestForChangedText(text_without_short_words[obecny]));
-                //});
-            //}
+            while (obecny<=text_without_short_words.length) {
+                requestForChangedText(text_without_short_words[obecny]);
+                przeskocz = getRandomInt(2,10);
+                obecny += przeskocz;
+            }
+        });
             
-            
-            text_without_short_words.forEach(function(element)
-            {
-                alert("Slowo: " + element);
-                requestForChangedText(element);
-            });
-            //splitted_text.length;
-            //alert("first word: " + splitted_text[0]);
+        text_without_short_words.forEach(function(element)
+        {
+            requestForChangedText(element);
         });
     }, 'r');
 });
 function requestForChangedText(text) {
     var res = GM_xmlhttpRequest({
             method: "POST",
-            url: "http://localhost:8080/sentence-find-near",
+            url: "http://172.27.0.103:8080/sentence-find-near",
             data: text,
-            synchronous: true,
+            synchronous: false,
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded"
             },
             onload: function(response) {
-                //alert("Response: " + response);
-                //res = response.responseText;
-                console.log("res: " + response.responseText);
                 document.body.innerHTML = document.body.innerHTML.replace(text, response.responseText);
-                //var json = $.parseJSON(response); 
             }
         });
-    //alert("res: " + res);
-    console.log(res);
     return res.responseText;
 }
-
-/*function requestForChangedText(text) {
-    var res = "";
-		$.ajax({
-				url: "http://localhost:8080/sentence-find-near",
-				method: "POST",
-            async: true,
-				dataType: "text",
-				data: text,
-				success: function(data) {
-                    res = data;
-                }
-		});
-        return res;
-}*/
